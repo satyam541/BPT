7165e90c1a9bcd881b4a944870f82060c5497980
sumanpreet kaur <sumanpreet.kaur@themsptraining.com>
2021-02-12 17:55:20 +0530

    coded banner and modified navbar

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="{{url('style/main.min.css')}}">
+    <link rel="stylesheet" href="{{url('style/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{url('style/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+</head>
+<body>
+    @yield("content")
+
+</body>
+
+<script src="{{url('script/main.js')}}"></script>
+<script src="{{url('script/owl.carousel.min.js')}}"></script>
+</html>
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,71 @@
+<div class="navbar" id="fixheader">
+    <div class="container">
+        <a href="javascript:void(0)" class="bpt-logo">
+            <img src="{{url('img/master/bpt-logo.svg')}}" alt="bptlogo">
+        </a>
+        <div class="menu" id="menuToggle" onclick="toggleMenu()">
+            <img src="{{url('img/master/menu.svg')}}" alt="menu">
+        </div>
+        <div class="menu-links">
+            <a href="javascript:void(0)" class="menu-toggle" onclick="toggleMenu()">
+                <img src="{{url('img/master/back.svg')}}" alt="back"> Back
+            </a>
+            <ul>
+                <li class="links-li">
+                    <a data-href="#overview" class="link smoothscroll">Certification</a>
+                </li>
+                <li class="links-li">
+                    <a data-href="#course" class="link smoothscroll">Courses</a>
+                </li>
+                <li class="links-li">
+                    <a data-href="#choose" class="link smoothscroll">Onsite</a>
+                </li>
+                <li class="links-li">
+                    <a data-href="#faq" class="link smoothscroll">About</a>
+                </li>
+                <li class="links-li">
+                    <a data-href="#azure-other" class="link smoothscroll">Locations</a>
+                </li>
+                <li class="links-li">
+                    <a class="search">
+                        <img src="{{url('img/master/search.svg')}}" alt="search">
+                    </a>
+                    <a class="cart">
+                        <img src="{{url('img/master/cart.svg')}}" alt="cart">
+                    </a>
+                    <a class="cart">
+                        <img src="{{url('img/flag/at.svg')}}" alt="cart">
+                    </a>
+                </li>
+                
+            </ul>
+            <ul class="top-bar">
+                <li>
+                    <span>
+                        <img src="{{url('img/master/call-us.svg')}}" alt="call us">
+                    </span>
+                    <div class="info">
+                        <p>Call Us:</p>
+                        <a>023 8000 1008</a>
+                    </div>
+                </li>
+                <li>
+                    <span>
+                        <img src="{{url('img/master/email.svg')}}" alt="email">
+                    </span>
+                    <div class="info">
+                        <p>Send Us Mail:</p>
+                        <a>info@thebestpracticetraining.com</a>
+                    </div>
+                </li>
+
+                <li class="buttons">
+                    <a class="btn-blue">
+                        <img src="{{url('img/master/quote.svg')}}" alt="quote">
+                        get a quote
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
