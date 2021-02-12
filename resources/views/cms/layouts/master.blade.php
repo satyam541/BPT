406e51f234335a642f796563dcfa54751b04770b
mehakdeep7 <mehakdeep.kaur@themsptraining.com>
2021-02-12 16:21:12 +0530

    master

--- resources/views/cms/layouts/master.blade.php
+++ resources/views/cms/layouts/master.blade.php
@@ -198,7 +198,7 @@
               </li>
             </ul>
           </li>
-          
+
 
         </ul>
       </nav>
@@ -257,14 +257,70 @@ $.widget.bridge('uibutton', $.ui.button)
 
 <script type="text/javascript" src="{{url('adminlte/DataTables/datatables.min.js')}}"></script>
 @yield('footer')
-
 <script>
-  $(function () {
-    // Summernote
-    $('#summernote').summernote()
-    
+  $(document).ready(function() {
+    $(document).on('click', function (event) {
+           $target = $(event.target);
+
+          });
+      $('#summernote').summernote({
+        toolbar:[
+          ['cleaner',['cleaner']], // The Button
+          ['style',['style']],
+          ['font',['bold','italic','underline','clear']],
+          ['fontname',['fontname']],
+          ['color',['color']],
+          ['para',['ul','ol','paragraph']],
+          ['height',['height']],
+          ['table',['table']],
+          ['insert',['media','link','hr']],
+          ['view',['fullscreen','codeview']],
+          ['help',['help']],
+          ['picture']
+      ],
+      cleaner:{
+            action: 'button', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
+            newline: '<br>', // Summernote's default is to use '<p><br></p>'
+            notStyle: 'position:absolute;top:0;left:0;right:0', // Position of Notification
+            icon: '<i class="note-icon">clean</i>',
+            keepHtml: true, // Remove all Html formats
+            keepOnlyTags: ['<p>', '<ul>', '<li>', '<a>','<h3>','<h4>','<h5>','<img>','<ol>','<span>'], // If keepHtml is true, remove all tags except these
+            keepClasses: true, // Remove Classes
+            badTags: ['style', 'script', 'applet', 'embed', 'noframes', 'noscript', 'html'], // Remove full tags with contents
+            badAttributes: ['style', 'start','color','bgcolor'], // Remove attributes from remaining tags
+            limitChars: false, // 0/false|# 0/false disables option
+            limitDisplay: 'both', // text|html|both
+            limitStop: false // true/false
+      },
+        height: 200,
+        callbacks: {
+            onImageUpload: function(image) {
+              var data = new FormData();
+              data.append("image", image[0]);
+              $.ajax({
+                  url:"{{route('ImageUpload')}}",
+                  headers: {
+                        'X-CSRF-TOKEN': $('meta[name="token"]').attr('content')
+                    },
+                  cache: false,
+                  contentType: false,
+                  processData: false,
+                  data: data,
+                  type: "post",
+                  success: function(url) {
+
+                    var imagetag = $('<img>').attr('src',url);
+                    $($target).summernote("insertNode", imagetag[0]);
+                  },
+                  error: function(data) {
+                      console.log(data);
+                  }
+              });
+            }
+        }
+      });
+
     });
-  })
-</script>
+    </script>
 </body>
 </html>
