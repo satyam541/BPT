01b078ae65ee0714839ae9dba5cbe4c207d36283
Arshdeep Singh <arshdeep.singh@themsptraining.com>
2021-02-12 11:38:49 +0530

    Changes for cms routes

---
+++ routes/cms.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| CMS Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register CMS routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "Web and Auth" middleware group. Now create something great!
+|
+*/
