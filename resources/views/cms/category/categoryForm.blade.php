6bad225613e41a643b7e21da19ecc32411a43119
Arshdeep Singh <arshdeep.singh@themsptraining.com>
2021-02-12 15:01:04 +0530

    category from design fixed

--- resources/views/cms/category/categoryForm.blade.php
+++ resources/views/cms/category/categoryForm.blade.php
@@ -1,110 +1,86 @@
 @extends('cms.layouts.master')
-@section('content-header')
-<h1>
-  Category
-  <small>Form</small>
-</h1>
-@endsection
 @section('content')
-<div class="content-wrapper">
-<section class="content">
-    <div class="row">
-            {{-- @if($category->name!=null)
-            {{ Breadcrumbs::render('edit_category',$category) }}
-        @else
-            {{ Breadcrumbs::render('add_category') }}
-        @endif --}}
-        <div class="container-fluid">
-            <div class="row">
-              <!-- left column -->
-              <div class="col-md-6">
-                <!-- general form elements -->
-                <div class="card card-primary">
-                  <div class="card-header">
-                    <h3 class="card-title">Category Form</h3>
-                  </div>
-                  <!-- /.card-header -->
-                  <!-- form start -->
-                  {{Form::model($category,['route'=>$submitRoute,"files"=>"true"])}}
-                    <div class="card-body">
-                      <div class="form-group">
-                        {{Form::label('name','Name',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::text('name',null,['class'=>'form-control'])}}
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('reference','Reference',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::text('reference',null,['class'=>'form-control'])}}
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('tag_line','Tag Line',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::textarea('tag_line',null,['class'=>'form-control editable'])}}
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('is_online','Is Online',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::checkbox('is_online')}}
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('color_code','Color Code',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::text('color_code',null,['class'=>'form-control colorInputJs','title'=>'Any valid color_code that work for css'])}}
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('image','Image',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::file('image',null,['class'=>'form-control'])}}
-                        <img src="{{ $category->getImagePath() }}" class=" pad" style="max-width: 50%" />
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('icon','Icon',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::file('icon',null,['class'=>'form-control'])}}
-                        <img src="{{ $category->getIconPath() }}" class=" pad" style="max-width:50%;" />
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('is_technical','is technical',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::checkbox('is_technical')}}
-                      </div>
-                      <div class="form-group">
-                        {{Form::label('published','Is Published',['class'=>'col-md-3 col-form-label text-right'])}}
-                        {{Form::checkbox('published')}}
-                      </div>
 
-                    </div>
-                    <!-- /.card-body -->
-    
-                    <div class="card-footer">
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                    {{Form::close()}}
-                </div>
+
+ <!-- Content Wrapper. Contains page content -->
+ <div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Category Form</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="#">Category</a></li>
+            <li class="breadcrumb-item"><a href="#">Form</a></li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Category Form</h3>
               </div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form>
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputFile">File input</label>
+                    <div class="input-group">
+                      <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="exampleInputFile">
+                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                      </div>
+                      <div class="input-group-append">
+                        <span class="input-group-text">Upload</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
             </div>
-        </div>
-
-    {{-- @if(!empty($category->id))
-    @php $faqModule = $category; $module_type='category'; @endphp
-        @include('cms.layouts.faqForm')
-    @endif --}}
-    </div><!-- row -->
-   
-</section>
-</div>                    
-@endsection
-@section('footerScripts')
-
-<script>
-    $(document).ready(function(){
-
-        $(".colorInputJs").on("change",function(){
-            $(this).css("background-color",$(this).val());
-        });
-        
-        $.each($(".colorInputJs"),function(index,object){
-            $(object).css("background-color",$(object).val());
-        });
-        $('#name').on('focusout',function(){
-            var str = $(this).val();
-            slug = convertUrl(str);
-            $("#reference").val(slug);
-        });
-    });
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+      </div>
+      <!-- /.row -->
+     
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
     
-</script>
 @endsection
