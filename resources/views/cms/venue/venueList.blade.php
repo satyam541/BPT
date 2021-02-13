c7d4286018eb415d2564adc1f61efb6bec49522a
sulika.sharma <sulika.sharma@themsptraining.com>
2021-02-13 16:43:31 +0530

    venue

--- resources/views/cms/venue/venueList.blade.php
+++ resources/views/cms/venue/venueList.blade.php
@@ -58,7 +58,7 @@
                 
                 </tfoot>
               </table>
-              <a id="add" href="" class="btn btn-success" style="">Add new record</a>
+              <a id="add" href="" class="btn btn-success" style="">Add new Record</a>
 
             </div>
             <!-- /.card-body -->
