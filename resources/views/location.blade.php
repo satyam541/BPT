25883bbdb2a8bbfc58ab4dd72608856062c1af18
Shivam <shivam.mahajan@themsptraining.com>
2021-02-18 10:02:52 +0530

    location page

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -18,7 +18,7 @@
         </div>
     </div>
 </section>
-<!-- End Banner Section -->
+<!-- End Banner Section -->  
 
 <!-- Start Convenient Section -->
 <section class="flex-container convenient">
@@ -125,7 +125,7 @@
         </div>
     </div>
 </section>
-<!-- End Convenient Section -->
+<!-- End Convenient Section --> 
 
 <!-- Start Find Section -->
 <section class="flex-container find">
@@ -375,71 +375,71 @@
     <div class="container">
         <div class="form-container">
         <form class="form" id="prince2-other">
-                        <div class="heading center-heading">
-                            <h2>Get In Touch <span>With Us Today</span> </h2>
+            <div class="heading center-heading">
+                <h2>Get In Touch With <span>Us</span> </h2>
+            </div>
+            <div class="form-input">
+                <div class="input-container">
+                    <span><img src="{{url('img/location/name.svg')}}" alt="name" class="black">
+                    <img src="{{url('img/location/name-red.svg')}}" alt="name-red" class="red"></span>
+                    <input type="text" name="f-name" id="f-name" placeholder="First Name*"
+                        autocomplete="off">
+                </div>
+                <div class="input-container">
+                    <span><img src="{{url('img/location/email.svg')}}" alt="email" class="black">
+                    <img src="{{url('img/location/email-red.svg')}}" alt="email-red" class="red"></span>
+                    <input type="text" name="email" id="email" placeholder="Email*" autocomplete="off">
+                </div>
+                <div class="input-container">
+                    <span><img src="{{url('img/location/phone-call.svg')}}" alt="phone-call" class="black">
+                    <img src="{{url('img/location/phone-callred.svg')}}" alt="phonecall-red" class="red"></span>
+                    <!-- <input type="number" name="phone" id="phone" placeholder="Phone Number*" autocomplete="off"> -->
+                    <div class="phonecode-field">
+                        <select class="country-code"></select>
+                        <span class="prefix"></span>
+                        <input type="number" class="telephone" placeholder="Phone Number*">
+                        <div style="z-index:-1;width:0;height:0;pointer-events: none;">
+                            <input type="text" name="Phone" class="phonenumber">
                         </div>
-                        <div class="form-input">
-                            <div class="input-container">
-                                <span><img src="{{url('img/contactus/name.svg')}}" alt="name" class="black">
-                                <img src="{{url('img/contactus/name-red.svg')}}" alt="name-red" class="red"></span>
-                                <input type="text" name="f-name" id="f-name" placeholder="First Name*"
-                                    autocomplete="off">
-                            </div>
-                            <div class="input-container">
-                                <span><img src="{{url('img/contactus/email.svg')}}" alt="email" class="black">
-                                <img src="{{url('img/contactus/email-red.svg')}}" alt="email-red" class="red"></span>
-                                <input type="text" name="email" id="email" placeholder="Email*" autocomplete="off">
-                            </div>
-                            <div class="input-container">
-                                <span><img src="{{url('img/contactus/phone-call.svg')}}" alt="phone-call" class="black">
-                                <img src="{{url('img/contactus/phone-callred.svg')}}" alt="phonecall-red" class="red"></span>
-                                <!-- <input type="number" name="phone" id="phone" placeholder="Phone Number*" autocomplete="off"> -->
-                                <div class="phonecode-field">
-                                    <select class="country-code"></select>
-                                    <span class="prefix"></span>
-                                    <input type="number" class="telephone" placeholder="Phone Number*">
-                                    <div style="z-index:-1;width:0;height:0;pointer-events: none;">
-                                        <input type="text" name="Phone" class="phonenumber">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="input-container">
-                                <span><img src="{{url('img/contactus/house.svg')}}" alt="house" class="black">
-                                <img src="{{url('img/contactus/house-red.svg')}}" alt="house-red" class="red"></span>
-                                <input type="text" name="address" id="adress" placeholder="Address"
-                                    autocomplete="off">
-                            </div>
-                            <div class="input-container">
-                                <span><img src="{{url('img/contactus/comment.svg')}}" alt="comment" class="black">
-                                <img src="{{url('img/contactus/comment-red.svg')}}" alt="comment-red" class="red"></span>
-                                <textarea placeholder="Message (Optional)" id="message" name="message"></textarea>
-                            </div>
-                        </div>
-                        <div class="form-consent">
-                            <p>The information you provide shall be processed by Pearce mayfield – a professional training organisation. Your data shall be used by a member of staff to contact you regarding your enquiry.
-                            </p>
-                        </div>
-                        <div class="form-consent">
-                            <p>Please click <a>here</a> for privacy policy. </p>
-                        </div>
-                        <div class="form-consent">
-                            <input type="checkbox" id="checkConsent">
-                            <label for="checkConsent">By submitting this enquiry I agree to be contacted in the most suitable manner (by phone or email) in order to respond to my enquiry.</label>
-                        </div>
-                        <div class="consent-error" style="display: none;">
-                            <p>We cannot process your enquiry without contacting you, please tick to confirm you
-                                consent to us contacting you about your enquiry</p>
-                        </div>
-                        <div class="form-consent">
-                            <input type="checkbox" name="marketing_consent" id="allowconsent">
-                            <label for="allowconsent">Click here to sign up to our email marketing, offers and discounts</label>
-                        </div>
-                        <div class="buttons">
-                            <button class="btn-blue">
-                                Submit
-                            </button>
-                        </div>
-                    </form>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <span><img src="{{url('img/location/house.svg')}}" alt="house" class="black">
+                    <img src="{{url('img/location/house-red.svg')}}" alt="house-red" class="red"></span>
+                    <input type="text" name="address" id="adress" placeholder="Address"
+                        autocomplete="off">
+                </div>
+                <div class="input-container">
+                    <span><img src="{{url('img/location/comment.svg')}}" alt="comment" class="black">
+                    <img src="{{url('img/location/comment-red.svg')}}" alt="comment-red" class="red"></span>
+                    <textarea placeholder="Message (Optional)" id="message" name="message"></textarea>
+                </div>
+            </div>
+            <div class="form-consent">
+                <p>The information you provide shall be processed by Pearce mayfield – a professional training organisation. Your data shall be used by a member of staff to contact you regarding your enquiry.
+                </p>
+            </div>
+            <div class="form-consent">
+                <p>Please click <a>here</a> for privacy policy. </p>
+            </div>
+            <div class="form-consent">
+                <input type="checkbox" id="checkConsent">
+                <label for="checkConsent">By submitting this enquiry I agree to be contacted in the most suitable manner (by phone or email) in order to respond to my enquiry.</label>
+            </div>
+            <div class="consent-error" style="display: none;">
+                <p>We cannot process your enquiry without contacting you, please tick to confirm you
+                    consent to us contacting you about your enquiry</p>
+            </div>
+            <div class="form-consent">
+                <input type="checkbox" name="marketing_consent" id="allowconsent">
+                <label for="allowconsent">Click here to sign up to our email marketing, offers and discounts</label>
+            </div>
+            <div class="buttons">
+                <button class="btn-blue">
+                    Submit
+                </button>
+            </div>
+        </form>
         </div>
     </div>
 </section>
