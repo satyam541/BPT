fb9ddbf944f70072425abf049b0061e68bc8fa12
sulika.sharma <sulika.sharma@themsptraining.com>
2021-02-18 10:02:34 +0530

    policies

--- resources/views/cms/article/article.blade.php
+++ resources/views/cms/article/article.blade.php
@@ -14,8 +14,8 @@
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-            <li class="breadcrumb-item"><a href="#">{{$type}}</a></li>
+            <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Dashboard</a></li>
+            <li class="breadcrumb-item active">{{$type}}</li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
