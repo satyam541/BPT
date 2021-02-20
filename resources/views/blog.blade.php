3277f972da54025653ae26a5b8501d1011e13d72
Shobit Thakur <shobit.thakur@themsptraining.com>
2021-02-20 14:43:05 +0530

    modification in blog page

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -8,14 +8,16 @@
     <div class="container">
         @include("layouts.navbar")
         <div class="banner-container">
-            <h1>Technical IT Certifications</h1>
-            <p>BPT was founded over 20 years ago with one simple mission: Finding the most trusted training courses around, at the most competitive prices. We recognise that the training marketplace is crowded.BPT was founded over 20 years ago with one simple mission.BPT was founded over 20 years ago with one simple mission.</p>
+            <h1>Blog</h1>
+            <p>Choose from over 200 courses which cover all aspects of business and 
+                personal training, including Project Management, IT Security, Business 
+                and many more. Our courses cater to every training need, from</p>
             <div class="breadcrums">
                 <ul>
                     <li><a href="">Home</a></li>
                     <img src="{{url('img/master/breadcrum-arrow.svg')}}" alt="breadcrums" class="white">
                     <img src="{{url('img/master/breadcrum-black.svg')}}" alt="breadcrums" class="black">
-                    <li><a href="">Technical IT Certifications</a></li>
+                    <li><a href="">Blog</a></li>
                 </ul>
             </div>
         </div>
