242eb0d950c96eefb704933c36fa85aabfcac7ab
Abhishek Kumar <abhishek.kumar@themsptraining.com>
2021-02-24 12:28:21 +0530

    Completed enquire popup.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -87,6 +87,78 @@
         </div>
     </footer>
 
+    <section class="flex-container popup">
+        <div class="container">
+            <div class="enquire-popup">
+                <span class="cross"><img src="{{url('img/master/cross.svg')}}" alt="name"></span>
+                {{-- <form class="form" id="popup"> --}}
+                        <form class="form sixsigma-co-uk-hubspot" onsubmit="submitEnquiry(this)" id="contact-us">
+                        
+                        @csrf
+                        <input type="hidden" name="type" value="contact"> 
+                        <input type="hidden" name="Url" id="url" value="{{Request::url()}}">
+
+                        <div class="heading center-heading white-heading">
+                            <h2>Get A Quote</h2>
+                        </div>
+                        <div class="form-input">
+                            <div class="input-container">
+                                <span><img src="{{url('img/contactus/name.svg')}}" alt="name" class="black">
+                                <img src="{{url('img/contactus/name-red.svg')}}" alt="name-red" class="red"></span>
+                                <input type="text" name="name" id="f-name" placeholder="Name*"
+                                    autocomplete="off">
+                            </div>
+                            <div class="input-container">
+                                <span><img src="{{url('img/contactus/phone-call.svg')}}" alt="phone-call" class="black">
+                                <img src="{{url('img/contactus/phone-callred.svg')}}" alt="phonecall-red" class="red"></span>
+                                <!-- <input type="number" name="phone" id="phone" placeholder="Phone Number*" autocomplete="off"> -->
+                                <div class="phonecode-field field-black">
+                                    <select class="country-code"></select>
+                                    <span class="prefix"></span>
+                                    <input type="number" class="telephone" placeholder="Phone Number*" min=0>
+                                    <div style="z-index:-1;width:0;height:0;pointer-events: none;">
+                                        <input type="text" name="phone" class="phonenumber" tabindex="-1">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="input-container">
+                                <span><img src="{{url('img/contactus/email.svg')}}" alt="email" class="black">
+                                <img src="{{url('img/contactus/email-red.svg')}}" alt="email-red" class="red"></span>
+                                <input type="text" name="email" id="email" placeholder="Email*" autocomplete="off">
+                            </div>
+                            <div class="input-container">
+                                <span><img src="{{url('img/contactus/comment.svg')}}" alt="comment" class="black">
+                                <img src="{{url('img/contactus/comment-red.svg')}}" alt="comment-red" class="red"></span>
+                                <textarea placeholder="Message (Optional)" id="message" name="message"></textarea>
+                            </div>
+                            <div class="form-consent">
+                            <p>The information you provide shall be processed by Best Practice Training Limited – a professional training organisation. Your data shall be used by a member of staff to contact you regarding your enquiry.
+                            </p>
+                        </div>
+                        <div class="form-consent">
+                            <p>Please click <a>here</a> for privacy policy. </p>
+                        </div>
+                        <div class="form-consent">
+                            <input type="checkbox" id="checkConsent" name="contactConsent">
+                            <label for="checkConsent">By submitting this enquiry I agree to be contacted in the most suitable manner (by phone or email) in order to respond to my enquiry.</label>
+                        </div>
+                        <div class="consent-error" style="display: none;">
+                            <p>We cannot process your enquiry without contacting you, please tick to confirm you
+                                consent to us contacting you about your enquiry</p>
+                        </div>
+                        <div class="form-consent">
+                            <input type="checkbox" name="marketing_consent" id="allowconsent">
+                            <label for="allowconsent">Click here to sign up to our email marketing, offers and discounts</label>
+                        </div>
+                        <div class="buttons">
+                            <button class="btn-blue" onclick="EnquiryFormSubmit('enquiry',this)">Submit</button>
+                            {{-- <button class="btn-blue">Submit</button> --}}
+                        </div>
+                    </form>
+                </div>
+        </div>
+    </section>
+
 </body>
 <!--enquiry submit script start-->
 <script>
