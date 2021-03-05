a8726d507519d2274694d631cb9174d7b17df739
Arshdeep Singh <arshdeep.singh@themsptraining.com>
2021-03-05 17:47:17 +0530

    datatable issue fixed

--- resources/views/cms/addon/courseAddonsList.blade.php
+++ resources/views/cms/addon/courseAddonsList.blade.php
@@ -78,10 +78,9 @@
         $(document).ready(function() {
             $('#example1').DataTable({
                 "columns": [
-                    { "name": "Type" },
-                    { "name": "Description" },
                     { "name": "Name" },
-                    { "name": "Action", "sorting": false, searching: false },
+                    { "name": "Type" },
+                    { "name": "Description" }
                 ]
             });
         });
