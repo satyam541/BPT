1278676fb6dfb2c550735b5968d502738728635e
satyam-suri <satyam.suri@themsptraining.com>
2021-03-06 13:21:33 +0530

    navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -65,7 +65,7 @@
                 </li>
 
                 <li class="buttons">
-                    <a class="btn-blue open-popup enquiryJS">
+                    <a data-type="top" class="btn-blue open-popup enquiryJS">
                         <img src="{{url('img/master/quote.svg')}}" alt="quote">
                         Get a Quote
                     </a>
