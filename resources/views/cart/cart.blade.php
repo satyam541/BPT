396e41ed1f97375f39e9a4eda78f27de5d460f3d
sulika-msp <sulika.sharma@themsptraining.com>
2021-03-11 11:14:04 +0530

    cart blade

--- resources/views/cart/cart.blade.php
+++ resources/views/cart/cart.blade.php
@@ -144,7 +144,7 @@
                         </div>
                     </div>
                     <div class="buttons">
-                        <a class="btn-white">
+                        <a href="{{route('catalouge')}}" class="btn-white">
                             Continue Shopping
                             <img src="{{ url('img/emptycart/shopping.svg') }}" alt="right-arrow">
                         </a>
