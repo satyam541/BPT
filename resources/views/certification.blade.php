e487dcd1eb882b8070bb27b96c77c11f112d2d2d
Arshdeep Singh <arshdeep.singh@themsptraining.com>
2021-03-11 15:36:12 +0530

    certification and home route fixed

--- resources/views/certification.blade.php
+++ resources/views/certification.blade.php
@@ -32,8 +32,9 @@
             <div class="course-list">
                 <div class="course-content" onclick="location.href = '{{ url('certification-details'.$certification->slug) }}';">
                     <a href="javascript:void(0);">{{$certification->name}}</a>
+                    {{-- {{ dd($certification->slug)}} --}}
                     <div class="buttons">
-                        <a href="{{url('certification-details'.$certification->slug)}}" class="btn-blue" >
+                        <a href="{{route('certificationDetail', ['certification'=>$certification->slug])}}" class="btn-blue" >
                             <img src="{{url('img/certification/detail.svg')}}" alt="detail">Certification
                         </a>
                     </div>
