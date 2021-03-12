e00d0561dd7b708fdcc5fee59b722f9eb1729397
Arshdeep Singh <arshdeep.singh@themsptraining.com>
2021-03-12 17:58:14 +0530

    master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ metaData('title') }}</title>
     <meta name="description" content="{{ metaData('description') }} " />
