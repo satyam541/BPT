ee24040c2d14dcd3ef0dec8ebdbac92b7622de50
Abhishek Kumar <abhishek.kumar@themsptraining.com>
2021-03-17 09:35:24 +0530

    modification in location page.

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -73,7 +73,7 @@
                 <p>{{$pageDetail->find['find_container']->content}}</p>
             </div>
             <div class="buttons">
-                <a class="btn-blue open-popup enquiryJS" data-quote="Enquire Now" data-quote="Enquire Now" data-type="other">
+                <a class="btn-blue open-popup enquiryJS" data-quote="Enquire Now" data-heading="Enquire Now" data-type="other">
                     <img src="{{url('img/location/enquire.svg')}}" alt="enquire"> Enquire Now
                 </a>
             </div>
