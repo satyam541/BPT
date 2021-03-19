ca8e09b8d1b8888034238ef1d9cde22b5048d732
Konica Arora <konica.arora@themsptraining.com>
2021-03-19 12:11:38 +0530

    fix undefined index issue.

--- resources/views/cart/cartDetail.blade.php
+++ resources/views/cart/cartDetail.blade.php
@@ -98,7 +98,7 @@
                                     <div class="phonecode-field field-black">
                                         <select class="country-code"></select>
                                         <span class="prefix"></span>
-                                        <input type="number" class="telephone" placeholder="Phone Number*">
+                                        <input type="number" class="telephone mobile" placeholder="Phone Number*">
                                         <div style="z-index:-1;width:0;height:0;pointer-events: none;">
                                             <input type="text" name="phone" class="phonenumber">
                                             <input type="text" name="phonecode" class="phonecode">
@@ -118,9 +118,10 @@
                                         <span class="prefix"></span>
                                         <input type="number" class="telephone" placeholder="Telephone Number">
                                         <div style="z-index:-1;width:0;height:0;pointer-events: none;">
-                                            <input type="text" name="phone" class="phonenumber">
-                                            <input type="text" name="phonecode" class="phonecode">
-                                            <input type="text" name="m_code" class="countrycode" autocomplete="off">
+                                            <input type="text" name="telephone" class="phonenumber">
+                                            <input type="text" name="cphonecode" class="phonecode">
+                                            <input type="text" name="t_code" class="countrycode" autocomplete="off">
+                                          
                                         </div>
                                     </div>
                                 </div>
@@ -367,7 +368,7 @@
                                             <div class="phonecode-field field-black">
                                                 <select class="country-code c_code"></select>
                                                 <span class="prefix code"></span>
-                                                <input type="number" class="telephone tel" placeholder="Phone Number*" min=0 autocomplete="off">
+                                                <input type="number" class="telephone tel mobile" placeholder="Phone Number*" min=0 autocomplete="off">
                                                 <div style="z-index:-1;width:0;height:0;pointer-events: none;">
                                                     <input type="text" name="phone" class="phonenumber" tabindex="-1" autocomplete="off">
                                                     <input type="text" name="phonecode" class="phonecode" tabindex="-1" autocomplete="off">
@@ -385,8 +386,10 @@
                                                 <span class="prefix  "></span>
                                                 <input type="number" class="telephone " placeholder="Telephone Number*" min=0>
                                                 <div style="z-index:-1;width:0;height:0;pointer-events: none;">
-                                                    <input type="text" name="telephone" class="phonenumber" tabindex="-1" autocomplete="off">
-                                                    <input type="text" name="phonecode" class="phonecode" tabindex="-1" autocomplete="off">
+                                                   
+                                                     <input type="text" name="telephone" class="phonenumber">
+                                                    <input type="text" name="cphonecode" class="phonecode">
+                                                    <input type="text" name="t_code" class="countrycode" autocomplete="off">
                                                 </div>
                                             </div>
                                         </div>
