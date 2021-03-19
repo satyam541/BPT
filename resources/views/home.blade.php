ff694499eb50da017ed7a91d4567d190db71f308
satyam-suri <satyam.suri@themsptraining.com>
2021-03-19 10:01:15 +0530

    images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -265,11 +265,11 @@
                         <p>Locations Worldwide</p>
                     </div>
                     <div class="item">
-                        <img src="{{ url('img/home/event.svg') }}" alt="event">
+                        <img src=" {{url('uploads/page/catalogue_image_1615444428.svg')}}" alt="trainers">
                         <div class="fact-count">
-                            <h3 class="count-number" data-to="{{$statsdata->stats['events']->content}}" data-speed="3000">{!!$statsdata->stats['events']->content!!}</h3><span>+</span>
+                            <h3 class="count-number" data-to="{{$statsdata->stats['trainers']->content}}" data-speed="3000">{!!$statsdata->stats['trainers']->content!!}</h3><span>+</span>
                         </div>
-                        <p>Events</p>
+                        <p>Trainers</p>
                     </div>
                     <div class="item">
                         <img src="{{ url('img/home/countries.svg') }}" alt="countries">
