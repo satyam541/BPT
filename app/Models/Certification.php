8de23f189013521f9e77d00e8f7cfbd27c88b90d
Paramjot Saini <paramjot.saini@themsptraining.com>
2021-03-19 14:15:41 +0530

    changes
    changes related to certifications

--- app/Models/Certification.php
+++ app/Models/Certification.php
@@ -10,9 +10,45 @@ class Certification extends Model
     use SoftDeletes;
     protected $table = 'certification';
     protected $primaryKey = "id";
+    protected $appends = ['url'];
+    public $image_path = "/uploads/category/";
+    public $icon_path = "/uploads/category/";
 
-    public function topic()
+    public function topics()
     {
         return $this->hasMany('App\Models\CertificationTopic');
+    }
+
+    public function getUrlAttribute()
+    {
+        $reference =  $this->reference;
+        $url = 'certification-programmes/'.$reference;
+        if(country()->country_code != 'gb')
+        {
+            $url = country()->country_code."/".$url;
+        }
+        return url($url);
+    }
+    public function getImagePath()
+    {// check file exist then return default image.
+        $imageLink = url($this->image_path.$this->image);
+        if (file_exists(public_path($this->image_path.$this->image))) {
+            return $imageLink;
+        } else {
+             return url('/images/default.png');
+        }
+        return url('/uploads/category/categoryImage1563435608.png');
+    }
+
+    public function getIconPath()
+    {
+        $imageLink = url($this->icon_path.$this->icon);
+        if(!empty($this->icon))
+        {   
+            if (file_exists(public_path($this->icon_path.$this->icon))) {
+                return $imageLink;
+            }
+        } 
+        return url('/uploads/category/categoryImage1563435608.png');
     }
 }
